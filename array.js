d8ea95bc5c1eddc5ac0f7cd2e9d4df4e81aa7153
Yoshi <hiroluk@gmail.com>
2016-01-12 18:21:55 -0800

    Finished test

--- array.js
+++ array.js
@@ -1,11 +1,26 @@
 Array.prototype.find = function(fn) {
-  return "failure"
+  for (var i = 0; i < this.length; i++) {
+    if(fn(this[i])){
+      return this[i];
+    }
+  }
+  return undefined;
 }
 
 Array.prototype.findIndex = function(fn) {
-  return 100
+  for (var i = 0; i < this.length; i++) {
+    if (fn(this[i])){
+      return i;
+    }
+  }
+  return -1;
 }
 
 Array.prototype.includes = function(search) {
+  for (var i = 0; i < this.length; i++) {
+    if (search === this[i]){
+      return true;
+    }
+  }
   return false
 }
--- string.js
+++ string.js
@@ -1,3 +1,16 @@
 String.prototype.endsWith = function(search, position) {
-  return false
+  var endIdx, searchIdx = search.length - 1;
+  if (position){
+    endIdx = position - 1;
+  } else {
+    endIdx = this.length - 1;
+  }
+  while(searchIdx !== 0){
+    if (search[searchIdx] !== this[endIdx]){
+      return false;
+    }
+    searchIdx -= 1;
+    endIdx -= 1;
+  }
+  return true;
 }
